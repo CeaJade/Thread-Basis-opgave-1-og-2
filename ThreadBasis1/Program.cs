8b1751b7b488bcc7a0ad2312ddf2a822b0ee4067
Caspj <Caspj@CASPER>
2020-12-17 14:01:18 +0100

    Add project files.

---
+++ ThreadBasis1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadBasis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t1 = new Thread(Write);
+                Thread t2 = new Thread(Write);
+                t1.Start();
+                Thread.Sleep(1000);
+                t2.Start();
+            }
+        }
+
+        static void Write()
+        {
+            Console.WriteLine("C#-trådning er nemt!");
+        }
+    }
+}
